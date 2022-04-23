f2d4df639795f8d9717c938659353dc60ef06b96
Eric Thomas <bluebird6922@gmail.com>
2022-04-23 04:01:22 +0300

    price for agency

--- resources/js/pages/purchase/agency_checkout.vue
+++ resources/js/pages/purchase/agency_checkout.vue
@@ -5,6 +5,40 @@
         </div>
         <div class="container">
             <div class="content">
+                <div class="category_level">
+                    <h4>{{$t('category_level')}} <router-link class="change" :to="{name: $t('dealer.url')}">{{$t('change')}}</router-link></h4>
+                    <div style="padding-left: 20px;">
+                        <h6>
+                            {{$t('category_application_description')}} 
+                            <span class="category">{{category.app_name}}</span>.
+                        </h6>
+                        <h6>
+                            {{$t('category_select_description')}} 
+                            <span class="category">{{category.category_tab}}</span>.
+                        </h6>
+                        <h6>
+                            {{$t('category_price_description')}} 
+                            <span class="category">{{$t(category.price)}}</span>.
+                        </h6>
+                        <h6 style="color: #007FED;">
+                            {{$t('category_discount_price_description')}}
+                            {{$t('category_discount_rate_description')}} 
+                            <span class="category">{{$t(category.discount)}}%</span>, 
+                            {{$t('category_discounted_price_description')}} 
+                            <span class="category">{{$t(category.discount_price)}}</span>.
+                        </h6>
+                        <h6>
+                            {{$t('category_capacity_description')}} 
+                            <span class="category">
+                                {{category.capacity}}{{category.capacity_unit}}
+                            </span>.
+                        </h6>
+                        <h6>
+                            {{$t('category_period_description')}} 
+                            <span class="category">{{category.period_date}} {{$t('days')}}</span>.
+                        </h6>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
