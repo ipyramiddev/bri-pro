3c76a6e828b1937d8d7fb69ea9070b147ed315a3
Eric Thomas <bluebird6922@gmail.com>
2022-04-25 05:09:38 +0300

    update router

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -47,6 +47,7 @@ export default [
   //Purchase routes
   { path: '/checkout', name: 'checkout', component: page('purchase/checkout.vue') },
   { path: '/agency/checkout', name: 'agency_checkout', component: page('purchase/agency_checkout.vue') },
+  { path: '/agency/plan', name: 'agency_plan', component: page('purchase/agency_plan.vue') },
   { path: '/checkout/confirmation', name: 'confirmation', component: page('purchase/confirmation.vue') },
   { path: '/transaction_history', name: 'transaction_history', component: page('purchase/transaction_history.vue') },
   { path: '/purchase_list', name: 'purchase_list', component: page('purchase/purchase_list.vue') },
